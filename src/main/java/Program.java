219ed690a04df4e5c35614d78b8f67899f94fa8d
oleg <legik7127@gmail.com>
2023-03-03 20:50:02 +0600

    added aglorhythm O(n2)

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,19 +3,36 @@ import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        List<Integer> availableDivider = findAvailableDivider(12);
+        List<Integer> availableDivider = findSimpleNumbers(100);
         for (Integer integer : availableDivider) {
             System.out.println(integer);
         }
     }
-        public static List<Integer> findAvailableDivider(int number){
-            List<Integer> result = new ArrayList<>();
-            for (int i = 1; i < number; i++){
-                if (number % i == 0){
+
+    public static List<Integer> findAvailableDivider(int number) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                result.add(i);
+            }
+
+        }
+        return result;
+    }
+
+    public static List<Integer> findSimpleNumbers(int max) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= max; i++) {
+            boolean simple = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    simple = false;
+                }
+            }
+                if (simple) {
                     result.add(i);
                 }
-
             }
-            return result;
-        }
+        return result;
     }
+}
